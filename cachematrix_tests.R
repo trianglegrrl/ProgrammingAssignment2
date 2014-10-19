e7e1de8f36c618a140aabbc299f803f072dddb5d
Alaina Hardie <alaina@precisionnutrition.com>
2014-10-19 15:29:53 -0400

    Update Indentation in cachematrix_tests

--- cachematrix_tests.R
+++ cachematrix_tests.R
@@ -7,8 +7,8 @@
 ##
 ## To run test suite:
 ##
-##     source('cachematrix_tests.R')
-##     suppressWarnings(test.runTests())
+##         source('cachematrix_tests.R')
+##         suppressWarnings(test.runTests())
 ##
 ## Expected results defined at https://class.coursera.org/rprog-008/forum/thread?thread_id=174
 ##
@@ -20,60 +20,60 @@ source('cachematrix.R')
 
 # Set up test suite
 test.runTests <- function () {
-  message("Running tests...")
+    message("Running tests...")
 
-  cacheSolve = test.cacheSolve()
+    cacheSolve = test.cacheSolve()
 
-  if(cacheSolve)
-  {
-    message("Tests passed!")
-  }
+    if(cacheSolve)
+    {
+        message("Tests passed!")
+    }
 }
 
 ## Unit test for cacheSolve() function
 
 test.cacheSolve <- function () {
-    # Define the source matrix
-    sourceMatrix <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2, byrow = TRUE)
+        # Define the source matrix
+        sourceMatrix <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2, byrow = TRUE)
 
-    # Define the expected result
-    invertedMatrix <- matrix(c(-2, 1, 1.5, -0.5), nrow = 2, ncol = 2, byrow = TRUE)
+        # Define the expected result
+        invertedMatrix <- matrix(c(-2, 1, 1.5, -0.5), nrow = 2, ncol = 2, byrow = TRUE)
 
-    # Set up the cache and run the calculation
-    computedMatrix <- makeCacheMatrix(sourceMatrix)
-    result <- cacheSolve(computedMatrix)
+        # Set up the cache and run the calculation
+        computedMatrix <- makeCacheMatrix(sourceMatrix)
+        result <- cacheSolve(computedMatrix)
 
-    # Test #1, verifying the result is what is expected
-    message("cacheSolve()")
+        # Test #1, verifying the result is what is expected
+        message("cacheSolve()")
 
-    # Compare the actual and expected results, and report whether the test succeeded
-    comparisonResult <- all.equal(invertedMatrix, result)
-    if((typeof(comparisonResult) == 'logical') & comparisonResult == TRUE) {
-        message("    - Returned expected value.")
-    } else {
-        message("    - DID NOT return expected value.")
-        message(paste("      Expected: ", invertedMatrix))
-        message(paste("      Got: ", result))
-        return(FALSE)
-    }
+        # Compare the actual and expected results, and report whether the test succeeded
+        comparisonResult <- all.equal(invertedMatrix, result)
+        if((typeof(comparisonResult) == 'logical') & comparisonResult == TRUE) {
+                message("        - Returned expected value.")
+        } else {
+                message("        - DID NOT return expected value.")
+                message(paste("            Expected: ", invertedMatrix))
+                message(paste("            Got: ", result))
+                return(FALSE)
+        }
 
-    # Test #2, confirming that a repeat hit is pulled from the cache
-    result <- cacheSolve(computedMatrix)
+        # Test #2, confirming that a repeat hit is pulled from the cache
+        result <- cacheSolve(computedMatrix)
 
-    # Variable to describe whether we got the cache-hit warning() message
-    gotCacheHit <- FALSE
+        # Variable to describe whether we got the cache-hit warning() message
+        gotCacheHit <- FALSE
 
-    tryCatch({
-      cacheSolve(computedMatrix)
-    }, warning = function(w) {
-      gotCacheHit <<- TRUE
-    })
+        tryCatch({
+            cacheSolve(computedMatrix)
+        }, warning = function(w) {
+            gotCacheHit <<- TRUE
+        })
 
-    if(gotCacheHit) {
-      message("    - Pulled result from cache")
-      TRUE
-    } else {
-      message("    - DID NOT pull result from cache")
-      FALSE
-    }
+        if(gotCacheHit) {
+            message("        - Pulled result from cache")
+            TRUE
+        } else {
+            message("        - DID NOT pull result from cache")
+            FALSE
+        }
 }
